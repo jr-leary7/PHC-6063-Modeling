1121404845e864db24372ecea35f9e4af3f0b9cd
jr-leary7 <jrleary@live.unc.edu>
2022-03-26 20:28:30 -0400

    Entire targets framework

--- R/functions.R
+++ R/functions.R
@@ -5,14 +5,13 @@ read_diab_data <- function(file.dir = "./") {
     stop("File does not exist in the provided directory.")
   } else {
     dat <- readr::read_csv(filepath)
-    dat <- dat %>% dplyr::with_groups(Diabetes_012, dplyr::slice_sample, n = 200)
   }
   return(dat)
 }
 
 clean_diab_data <- function(diab.df = NULL) {
   # check input 
-  
+  if (is.null(diab.df)) { stop("clean_diab_data() needs non-NULL arguments.") }
   # clean data 
   diab.df <- diab.df %>% 
              dplyr::rename(DIAGNOSIS = Diabetes_012, 
@@ -43,4 +42,104 @@ clean_diab_data <- function(diab.df = NULL) {
                            MENTAL_HEALTH_BAD_LAST_30_DAYS = as.numeric(MENTAL_HEALTH_BAD_LAST_30_DAYS), 
                            PHYS_HEALTH_BAD_LAST_30_DAYS = as.numeric(PHYS_HEALTH_BAD_LAST_30_DAYS))
   return(diab.df)
+}
+
+tune_model <- function(mod.workflow = NULL, cv.folds = NULL, mod.type = NULL) {
+  # check inputs 
+  if (is.null(mod.workflow) | is.null(cv.folds)) { stop("tune_model() needs non-NULL arguments.") }
+  mod.type <- tolower(mod.type)
+  # tune model 
+  if (mod.type == "") {
+    # comment
+  } else if (mod.type == "rf") {
+    param_grid <- grid_latin_hypercube(list(mtry = mtry(c(1, floor(ncol(cv.folds$splits[[1]]$data) / 2))), 
+                                            trees = trees(), 
+                                            min_n = min_n()), 
+                                       size = 15)
+  } else if (mod.type == "xgboost") {
+    param_grid <- grid_latin_hypercube(list(trees = trees(), 
+                                            tree_depth = tree_depth(), 
+                                            min_n = min_n(), 
+                                            loss_reduction = loss_reduction(), 
+                                            sample_size = sample_prop(c(0.1, 0.6)), 
+                                            mtry = mtry(c(1, floor(ncol(cv.folds$splits[[1]]$data) / 2))), 
+                                            stop_iter = stop_iter(), 
+                                            learn_rate = learn_rate()), 
+                                       size = 15)
+  } else if (mod.type == "svm_poly") {
+    param_grid <- grid_latin_hypercube(list(cost = cost(),
+                                            degree = degree(),
+                                            scale_factor = scale_factor(),
+                                            margin = svm_margin()), 
+                                       size = 15)
+  } else if (mod.type == "svm_linear") {
+    param_grid <- grid_latin_hypercube(list(cost = cost(), margin = svm_margin()), 
+                                       size = 15)
+  } else if (mod.type == "svm_radial") {
+    param_grid <- grid_latin_hypercube(list(cost = cost(), 
+                                            rbf_sigma = rbf_sigma(),
+                                            margin = svm_margin()), 
+                                       size = 15)
+  } else if (mod.type == "knn") {
+    param_grid <- grid_latin_hypercube(list(neighbors = neighbors(), 
+                                            weight_func = weight_func(), 
+                                            dist_power = dist_power()), 
+                                       size = 15)
+  } else if (mod.type == "nn") {
+    param_grid <- grid_latin_hypercube(list(hidden_units = hidden_units(), 
+                                            penalty = penalty(), 
+                                            epochs = epochs()))
+  }
+  
+  tuning_metrics <- metric_set(roc_auc, 
+                               precision, 
+                               recall, 
+                               pr_auc, 
+                               kap, 
+                               sens, 
+                               spec, 
+                               ppv, 
+                               npv, 
+                               f_meas, 
+                               bal_accuracy)
+  tune_controls <- control_grid(save_pred = TRUE, 
+                                parallel_over = "resamples", 
+                                allow_par = TRUE)
+  if (mod.type == "naivebayes") {
+    tuning_res <- tune_grid(object = mod.workflow, 
+                            resamples = cv.folds, 
+                            metrics = tuning_metrics, 
+                            control = tune_controls)
+  } else {
+    tuning_res <- tune_grid(object = mod.workflow, 
+                            resamples = cv.folds, 
+                            grid = param_grid, 
+                            metrics = tuning_metrics, 
+                            control = tune_controls)
+  }
+  
+  return(tuning_res)
+}
+
+fit_best_train <- function(mod.workflow = NULL, best.params = NULL, train.dat = NULL) {
+  # check inputs 
+  if (is.null(mod.workflow) | is.null(best.params) | is.null(train.dat)) { stop("Arguments to fit_best_train() must be non-NULL.") }
+  # fit
+  training_res <- mod.workflow %>% 
+                  finalize_workflow(parameters = best.params) %>% 
+                  fit(data = train.dat)
+  return(training_res)
+}
+
+get_var_imp <- function(mod.fit = NULL, mod.type = "xgboost") {
+  # check inputs 
+  if (is.null(mod.fit)) { stop("mod.fit must be non-NULL.") }
+  mod.type <- tolower(mod.type)
+  # feature importance 
+  if (mod.type == "xgboost") {
+    var_imp <- xgboost::xgb.importance(model = mod.fit$fit$fit$fit)
+  } else {
+    var_imp <- ranger::importance(mod.fit$fit$fit$fit)
+  }
+  return(var_imp)
 }
--- _targets.R
+++ _targets.R
@@ -2,7 +2,7 @@
 
 library(targets)
 source("R/functions.R")
-tar_option_set(packages = c("tidyverse", "tidymodels", "magrittr", "xgboost", "ranger", "e1071", "klaR"))
+tar_option_set(packages = c("tidyverse", "tidymodels", "magrittr", "xgboost", "ranger", "kernlab", "klaR", "kknn", "discrim", "nnet"))
 
 ##### parallel computing #####
 cl <- parallel::makeCluster(4)
@@ -16,10 +16,199 @@ list(
   tar_target(diab_data_clean, clean_diab_data(diab.df = diab_data_raw)), 
   # set up train / test / validation sets as well as 10-fold cross-validation folds
   tar_target(diab_val_split, validation_split(data = diab_data_clean, prop = 0.85, strata = DIAGNOSIS)), 
-  tar_target(diab_val_data, assessment(diab_val_split)), 
-  tar_target(diab_analysis_data, analysis(diab_val_split)), 
-  tar_target(diab_tt_split, initial_split(data = diab_analysis_data, prop = 0.6, strata = DIAGNOSIS)), 
+  tar_target(diab_val_data, assessment(diab_val_split$splits[[1]])), 
+  tar_target(diab_analysis_data, analysis(diab_val_split$splits[[1]])), 
+  tar_target(diab_tt_split, initial_split(data = diab_analysis_data, prop = 0.82, strata = DIAGNOSIS)),  # ensures 70 / 15 / 15 overall split
   tar_target(diab_train, training(diab_tt_split)),
   tar_target(diab_test, testing(diab_tt_split)),
-  tar_target(diab_train_folds, vfold_cv(diab_train, v = 10, strata = DIAGNOSIS))
+  tar_target(diab_train_folds, vfold_cv(diab_train, v = 10, strata = DIAGNOSIS)), 
+  
+  # build multinomial naive Bayes model
+  tar_target(naive_bayes_mod, naive_Bayes() %>%
+                              set_mode("classification") %>%
+                              set_engine("klaR")),
+  tar_target(naive_bayes_recipe, recipe(DIAGNOSIS ~., data = diab_train) %>%
+                                 step_nzv(all_predictors(), freq_cut = 99/1) %>%
+                                 step_normalize(all_numeric_predictors())),
+  tar_target(naive_bayes_workflow, workflow() %>%
+                                   add_model(naive_bayes_mod) %>%
+                                   add_recipe(naive_bayes_recipe)),
+  # train multinomial naive Bayes model & generate predictions (no hyperparameters for this one)
+  tar_target(naive_bayes_fit, naive_bayes_workflow %>% fit(data = diab_train)), 
+  tar_target(naive_bayes_train_preds, predict(naive_bayes_fit, new_data = diab_train, type = "prob")),
+  tar_target(naive_bayes_test_preds, predict(naive_bayes_fit, new_data = diab_test, type = "prob")),
+  
+  # build KNN model
+  tar_target(knn_mod, nearest_neighbor(neighbors = tune(), 
+                                       weight_func = tune(), 
+                                       dist_power = tune()) %>% 
+                      set_mode("classification") %>% 
+                      set_engine("kknn")), 
+  tar_target(knn_recipe, recipe(DIAGNOSIS ~., data = diab_train) %>% 
+                         step_dummy(all_nominal_predictors()) %>%
+                         step_nzv(all_predictors(), freq_cut = 99/1) %>%
+                         step_normalize(all_numeric_predictors())),
+  tar_target(knn_workflow, workflow() %>%
+                           add_model(knn_mod) %>%
+                           add_recipe(knn_recipe)), 
+  # train knn model & generate predictions
+  tar_target(knn_tuning_res, tune_model(mod.workflow = knn_workflow, 
+                                        cv.folds = diab_train_folds,
+                                        mod.type = "knn")), 
+  tar_target(knn_best_params, select_best(knn_tuning_res, "roc_auc")),
+  tar_target(knn_fit, fit_best_train(mod.workflow = knn_workflow,
+                                     best.params = knn_best_params,
+                                     train.dat = diab_train)),
+  tar_target(knn_train_preds, predict(knn_fit, new_data = diab_train, type = "prob")),
+  tar_target(knn_test_preds, predict(knn_fit, new_data = diab_test, type = "prob")), 
+  
+  # build linear SVM model
+  tar_target(svm_linear_mod, svm_linear(cost = tune(), margin = tune()) %>%
+                             set_mode("classification") %>%
+                             set_engine("kernlab", scaled = FALSE)),
+  tar_target(svm_linear_recipe, recipe(DIAGNOSIS ~., data = diab_train) %>% 
+                                step_dummy(all_nominal_predictors()) %>%
+                                step_nzv(all_predictors(), freq_cut = 99/1) %>%
+                                step_normalize(all_numeric_predictors())),
+  tar_target(svm_linear_workflow, workflow() %>%
+                                  add_model(svm_linear_mod) %>%
+                                  add_recipe(svm_linear_recipe)),
+  # train linear SVM model & generate predictions
+  tar_target(svm_linear_tuning_res, tune_model(mod.workflow = svm_linear_workflow, 
+                                               cv.folds = diab_train_folds,
+                                               mod.type = "svm_linear")), 
+  tar_target(svm_linear_best_params, select_best(svm_linear_tuning_res, "roc_auc")),
+  tar_target(svm_linear_fit, fit_best_train(mod.workflow = svm_linear_workflow,
+                                            best.params = svm_linear_best_params,
+                                            train.dat = diab_train)),
+  tar_target(svm_linear_train_preds, predict(svm_linear_fit, new_data = diab_train, type = "prob")),
+  tar_target(svm_linear_test_preds, predict(svm_linear_fit, new_data = diab_test, type = "prob")), 
+  
+  # build polynomial SVM model
+  tar_target(svm_poly_mod, svm_poly(cost = tune(),
+                                    degree = tune(),
+                                    scale_factor = tune(),
+                                    margin = tune()) %>%
+                           set_mode("classification") %>%
+                           set_engine("kernlab", scaled = FALSE)),
+  tar_target(svm_poly_recipe, recipe(DIAGNOSIS ~., data = diab_train) %>% 
+                              step_dummy(all_nominal_predictors()) %>%
+                              step_nzv(all_predictors(), freq_cut = 99/1) %>%
+                              step_normalize(all_numeric_predictors())),
+  tar_target(svm_poly_workflow, workflow() %>%
+                                add_model(svm_poly_mod) %>%
+                                add_recipe(svm_poly_recipe)),
+  # train polynomial SVM model & generate predictions
+  tar_target(svm_poly_tuning_res, tune_model(mod.workflow = svm_poly_workflow, 
+                                             cv.folds = diab_train_folds,
+                                             mod.type = "svm_poly")), 
+  tar_target(svm_poly_best_params, select_best(svm_poly_tuning_res, "roc_auc")),
+  tar_target(svm_poly_fit, fit_best_train(mod.workflow = svm_poly_workflow,
+                                    best.params = svm_poly_best_params,
+                                    train.dat = diab_train)),
+  tar_target(svm_poly_train_preds, predict(svm_poly_fit, new_data = diab_train, type = "prob")),
+  tar_target(svm_poly_test_preds, predict(svm_poly_fit, new_data = diab_test, type = "prob")), 
+  
+  # build radial SVM model
+  tar_target(svm_radial_mod, svm_rbf(cost = tune(), 
+                                     rbf_sigma = tune(), 
+                                     margin = tune()) %>%
+                             set_mode("classification") %>%
+                             set_engine("kernlab", scaled = FALSE)),
+  tar_target(svm_radial_recipe, recipe(DIAGNOSIS ~., data = diab_train) %>% 
+                                step_dummy(all_nominal_predictors()) %>%
+                                step_nzv(all_predictors(), freq_cut = 99/1) %>%
+                                step_normalize(all_numeric_predictors())),
+  tar_target(svm_radial_workflow, workflow() %>%
+                                  add_model(svm_radial_mod) %>%
+                                  add_recipe(svm_radial_recipe)),
+  # train radial SVM model & generate predictions
+  tar_target(svm_radial_tuning_res, tune_model(mod.workflow = svm_radial_workflow, 
+                                               cv.folds = diab_train_folds,
+                                               mod.type = "svm_radial")), 
+  tar_target(svm_radial_best_params, select_best(svm_radial_tuning_res, "roc_auc")),
+  tar_target(svm_radial_fit, fit_best_train(mod.workflow = svm_radial_workflow,
+                                            best.params = svm_radial_best_params,
+                                            train.dat = diab_train)),
+  tar_target(svm_radial_train_preds, predict(svm_radial_fit, new_data = diab_train, type = "prob")),
+  tar_target(svm_radial_test_preds, predict(svm_radial_fit, new_data = diab_test, type = "prob")), 
+  
+  # build random forest model
+  tar_target(rf_mod, rand_forest(mtry = tune(), 
+                                 trees = tune(),
+                                 min_n = tune()) %>%
+                     set_mode("classification") %>%
+                     set_engine("ranger", importance = "impurity")), 
+  tar_target(rf_recipe, recipe(DIAGNOSIS ~., data = diab_train) %>% 
+                        step_dummy(all_nominal_predictors()) %>%
+                        step_nzv(all_predictors(), freq_cut = 99/1) %>%
+                        step_normalize(all_numeric_predictors())), 
+  tar_target(rf_workflow, workflow() %>% 
+                          add_model(rf_mod) %>% 
+                          add_recipe(rf_recipe)), 
+  # train random forest model & generate predictions
+  tar_target(rf_tuning_res, tune_model(mod.workflow = rf_workflow, 
+                                       cv.folds = diab_train_folds,
+                                       mod.type = "rf")), 
+  tar_target(rf_best_params, select_best(rf_tuning_res, "roc_auc")),
+  tar_target(rf_fit, fit_best_train(mod.workflow = rf_workflow,
+                                    best.params = rf_best_params,
+                                    train.dat = diab_train)),
+  tar_target(rf_train_preds, predict(rf_fit, new_data = diab_train, type = "prob")),
+  tar_target(rf_test_preds, predict(rf_fit, new_data = diab_test, type = "prob")), 
+  tar_target(rf_var_imp, get_var_imp(mod.fit = rf_fit, mod.type = "rf")), 
+  
+  # build xgboost model 
+  tar_target(xgboost_mod, boost_tree(trees = tune(),
+                                     tree_depth = tune(),
+                                     min_n = tune(),
+                                     loss_reduction = tune(),
+                                     sample_size = tune(),
+                                     mtry = tune(),
+                                     stop_iter = tune(),
+                                     learn_rate = tune()) %>% 
+                          set_mode("classification") %>% 
+                          set_engine("xgboost")), 
+  tar_target(xgboost_recipe, recipe(DIAGNOSIS ~., data = diab_train) %>% 
+                             step_dummy(all_nominal_predictors()) %>%
+                             step_nzv(all_predictors(), freq_cut = 99/1) %>%
+                             step_normalize(all_numeric_predictors())), 
+  tar_target(xgboost_workflow, workflow() %>% 
+                               add_model(xgboost_mod) %>% 
+                               add_recipe(xgboost_recipe)), 
+  # train xgboost model & generate predictions
+  tar_target(xgboost_tuning_res, tune_model(mod.workflow = xgboost_workflow, 
+                                            cv.folds = diab_train_folds, 
+                                            mod.type = "xgboost")), 
+  tar_target(xgboost_best_params, select_best(xgboost_tuning_res, "roc_auc")),
+  tar_target(xgboost_fit, fit_best_train(mod.workflow = xgboost_workflow,
+                                         best.params = xgboost_best_params,
+                                         train.dat = diab_train)),
+  tar_target(xgboost_train_preds, predict(xgboost_fit, new_data = diab_train, type = "prob")),
+  tar_target(xgboost_test_preds, predict(xgboost_fit, new_data = diab_test, type = "prob")), 
+  tar_target(xgboost_var_imp, get_var_imp(mod.fit = xgboost_fit, mod.type = "xgboost")), 
+  
+  # build neural network model 
+  tar_target(nn_mod, mlp(hidden_units = tune(), 
+                         penalty = tune(), 
+                         epochs = tune()) %>% 
+                     set_mode("classification") %>% 
+                     set_engine("nnet")), 
+  tar_target(nn_recipe, recipe(DIAGNOSIS ~., data = diab_train) %>% 
+                        step_dummy(all_nominal_predictors()) %>%
+                        step_nzv(all_predictors(), freq_cut = 99/1) %>%
+                        step_normalize(all_numeric_predictors())), 
+  tar_target(nn_workflow, workflow() %>% 
+                          add_model(nn_mod) %>% 
+                          add_recipe(nn_recipe)), 
+  # train neural network model & generate predictions
+  tar_target(nn_tuning_res, tune_model(mod.workflow = nn_workflow, 
+                                       cv.folds = diab_train_folds, 
+                                       mod.type = "nn")), 
+  tar_target(nn_best_params, select_best(nn_tuning_res, "roc_auc")),
+  tar_target(nn_fit, fit_best_train(mod.workflow = nn_workflow,
+                                    best.params = nn_best_params,
+                                    train.dat = diab_train)),
+  tar_target(nn_train_preds, predict(nn_fit, new_data = diab_train, type = "prob")),
+  tar_target(nn_test_preds, predict(nn_fit, new_data = diab_test, type = "prob"))
 )
